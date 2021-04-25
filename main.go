d909b183bb145a9e85f22e1b399e8f4bb05f06fd
svbnbyrk <svbnbyrk@gmail.com>
2021-04-25 06:38:34 +0300

    Completed module

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		
 		_, err := fmt.Scanln(&originValue)
 		if err!= nil{
 			printError(errReadingInput)
@@ -39,11 +39,12 @@ func main() {
 			convertToCelsius(originValue)
 		}
 
-		_ ,err := fmt.Scanln(&shouldConvertAgain)
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		_, err = fmt.Scanln(&shouldConvertAgain)
 		if err!= nil{
 			printError(errReadingInput)
 		} 
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
 		if strings.TrimSpace(strings.ToUpper(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
